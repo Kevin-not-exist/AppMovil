489cc2bea64c3d4a503e6bda9007dc4378421db7
Kevin-not-exist <kevin.devotto.posse@users.noreply.github.com>
2020-08-06 23:27:27 -0300

    Summary

--- js/codigo.js
+++ js/codigo.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
         const documento = $("#txt_documento").val();
         const pass = $("#txt_pass").val();
+        const passRepeat = $("#txt_pass_repeat").val();
         const email = $("#txt_email").val();
         const telefono = $("#txt_telefono").val();
         const direccion = $("#txt_direccion").val();
@@ -12,48 +13,63 @@ $(document).ready(function(){
 
         try{
             /*Validar si estan vacios*/
-            if(!documento || !pass || !email || !telefono || !direccion || !nombre || !apellido || !genero){
+            if(!documento || !pass || !email || !telefono || !direccion || !nombre || !apellido || !genero || !passRepeat){
                 throw "Debe completar todos los campos!";
             }
-        const url = `https://ort-api.herokuapp.com/usuarios/registro`;
-        $.ajax({
-            data:JSON.stringify(
-                {
-                "telefono": telefono,
-                "email":email,
-                "contrasenia":pass,
-                "genero":genero,
-                "apellido":apellido,
-                "nombre":nombre,
-                "documento":documento
-               }),
-
-            url:url,
-            type:"POST",
-            dataType:"json",
-            success:function(respuesta){
-                $.ajaxSetup({
-                    headers:{
-                        token:respuesta.token
-                    }
-                })
-                let usuario = respuesta.descripcion;
-                usuario = JSON.stringify(usuario);
-                sessionStorage.setItem("usuario", usuario);
-                ons.notification.toast("El usuario se registro correctamente", {"timeout":3000});
-                const nav = document.getElementById("nav");
-                nav.pushPage("t_info_medico");
-            },
-            error:function(respuesta_error, status, error){
-                ons.notification.toast(respuesta_error.responseJSON.descripcion, {"timeout":3000});
+            if(pass != passRepeat){
+                throw "Las contraseñas no coinciden.";
             }
-        })
+            if(isNaN(documento)){
+                throw "Formato documento incorrecto.";
+            }
+            let exp = /(-)*(\.)*/;
+            let expEmail = /[@]*(com)*/;
+            if(exp.test(documento)){
+                throw "El documento debe estar sin puntos ni guiones."
+            }
+            if(expEmail.test(email)){
+                throw "Formato E-mail incorrecto."
+            }
+            const url = `https://ort-api.herokuapp.com/usuarios/registro`;
+            $.ajax({
+                data:JSON.stringify(
+                    {
+                    "telefono": telefono,
+                    "email":email,
+                    "contrasenia":pass,
+                    "genero":genero,
+                    "apellido":apellido,
+                    "nombre":nombre,
+                    "documento":documento
+                }),
+
+                url:url,
+                type:"POST",
+                dataType:"json",
+                success:function(respuesta){
+                    $.ajaxSetup({
+                        headers:{
+                            token:respuesta.token
+                        }
+                    })
+                    let usuario = respuesta.descripcion;
+                    usuario = JSON.stringify(usuario);
+                    sessionStorage.setItem("usuario", usuario);
+                    ons.notification.toast("El usuario se registro correctamente", {"timeout":30000});
+                    const nav = document.getElementById("nav");
+                    nav.pushPage("t_info_medico");
+                },
+                error:function(respuesta_error, error, status){
+                    ons.notification.toast(respuesta_error.responseJSON.descripcion, {"timeout":30000});
+                }
+            });
         }
         catch(e){
-            ons.notification.toast(e, {"timeout":3000});
+            ons.notification.toast(e, {"timeout":30000});
         }
     })
 
+    /*
     $("#btn_no_registrado").click(function(){
         const nav = document.getElementById("nav");
         nav.pushPage("t_registro");
@@ -66,6 +82,6 @@ $(document).ready(function(){
 
         const nav = document.getElementById("nav");
         nav.pushPage("t_info_medico");
-    })
+    })*/
 
 })
