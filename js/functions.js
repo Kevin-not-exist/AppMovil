301e9e093be167a736f7f00487d874da8edefe27
Kevin-not-exist <kevin.devotto.posse@users.noreply.github.com>
2020-08-07 12:22:06 -0300

    Summary

--- js/functions.js
+++ js/functions.js
@@ -20,12 +20,11 @@ $(document).ready(function(){
             let exp = /^[0-9]{7,8}$/g;
             let expEmail = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\w+\.[a-zA-Z]+/g;
             if(!exp.test(documento)){
-                throw "El documento debe estar sin puntos ni guiones."
+                throw "El documento debe estar sin puntos ni guiones (7-8 digitos)."
             }
             if(!expEmail.test(email)){
                 throw "Formato E-mail incorrecto."
             }
-
             $.ajax({
                 url:url,
                 type:"POST",
@@ -41,6 +40,7 @@ $(document).ready(function(){
                         "documento": documento
                     }
                 ),
+                contentType: 'application/json; charset=utf-8',
                 success:function(respuesta){
                     $.ajaxSetup({
                         headers:{
@@ -58,7 +58,60 @@ $(document).ready(function(){
                 error:function(respuesta_error, err, status){
                     console.log(err);
                     console.log(status);
-                    ons.notification.toast(respuesta_error.responseJSON.error, {"timeout":3000});
+                    ons.notification.toast(respuesta_error.responseText, {"timeout":3000});
+                }
+            });
+        }
+        catch(e){
+            ons.notification.toast(e, {"timeout":3000});
+        }
+    });
+
+    $("#btn_login").on("click", function(){
+        const email_login   = $("#email_login").val();
+        const pwd_login     = $("#pwd_login").val();
+        const tipo_login    = $("#tipo_login").val();
+        const url           = "https://ort-api.herokuapp.com/usuarios/login";
+
+        try{
+            if(!email_login || !pwd_login || !tipo_login){
+                throw "Debe completar todos los campos para continuar."
+            }
+            let exp = /^[0-9]{7,8}$/g;
+            let expEmail = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\w+\.[a-zA-Z]+/g;
+            if(!expEmail.test(email_login)){
+                throw "Formato E-mail incorrecto."
+            }
+            $.ajax({
+                url:url,
+                type:"POST",
+                dataType:"json",
+                data:JSON.stringify(
+                    {
+                        "email": email_login,
+                        "contrasenia": pwd_login,
+                        "tipo": tipo_login
+                    }
+                ),
+                contentType: 'application/json; charset=utf-8',
+                success:function(respuesta){
+                    $.ajaxSetup({
+                        headers:{
+                            token:respuesta.token
+                        }
+                     });
+                    let usuario = respuesta.usuario;
+                    usuario     = JSON.stringify(usuario);
+                    sessionStorage.setItem("usuario", usuario);
+                    ons.notification.toast("El usuario se registro correctamente", {"timeout":3000});
+                    //redireccionar a listado de locales.
+                    const nav = document.getElementById("nav");
+                    nav.pushPage("t_info_medico");
+                },
+                error:function(respuesta_error, err, status){
+                    console.log(err);
+                    console.log(status);
+                    ons.notification.toast(respuesta_error.responseText, {"timeout":3000});
                 }
             });
         }
